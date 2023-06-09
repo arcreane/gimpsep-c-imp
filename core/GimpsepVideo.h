7ed89a5eded9d30b7346e8606d8714ff454764d5
witchaya_api <86241447+witchayaAPI@users.noreply.github.com>
2023-06-09 07:18:46 -0400

    Update GimpsepVideo.h

--- core/GimpsepVideo.h
+++ core/GimpsepVideo.h
@@ -11,22 +11,23 @@ class GimpsepVideo {
 private:
     static std::pair<cv::VideoCapture, cv::VideoWriter>
     readVideo(const String &filename, const String &outputPath, bool isColor);
+    static void showVideos(const String &inputVideo, const String &outputVideo);
 
 public:
-    static void dilate(String &inputPath, String &outputPath, int erosionSize);
+    static void dilate(String &inputPath, String &outputPath, int erosionSize, char verbose);
 
-    static void erode(String &inputPath, String &outputPath, int erosionSize);
+    static void erode(String &inputPath, String &outputPath, int erosionSize, char verbose);
 
-    static void resize(String &inputPath, String &outputPath, int width, int height);
+    static void resize(String &inputPath, String &outputPath, int width, int height, char verbose);
 
-    static void resize(String &inputPath, String &outputPath, double factor);
+    static void resize(String &inputPath, String &outputPath, double factor, char verbose);
 
-    static void lightenDarken(String &inputPath, String &outputPath, double factor);
+    static void lightenDarken(String &inputPath, String &outputPath, double factor, char verbose);
 
     static void cannyEdgeDetection(String &inputPath, String &outputPath, double threshold1, double threshold2,
-                                   int apertureSize);
+                                   int apertureSize, char verbose);
 
-    static void faceDetection(String &inputPath, String &outputPath, String cascadeModel);
+    static void faceDetection(String &inputPath, String &outputPath, String cascadeModel, char verbose);
 
     static void detectAndDraw(cv::CascadeClassifier &cls, cv::Mat &img);
 };
