ca8c61ce5965c17d2fedc1ad7be83a3070a75521
witchaya_api <86241447+witchayaAPI@users.noreply.github.com>
2023-06-09 07:18:18 -0400

    Update Gimpsep.h

--- core/Gimpsep.h
+++ core/Gimpsep.h
@@ -10,24 +10,25 @@ typedef std::string String;
 class Gimpsep {
 private:
     static cv::Mat readImage(const String &filename, int flags);
+    static void showImages(cv::Mat inputImage, cv::Mat outputImage);
 
 public:
-    static void dilate(String &inputPath, String &outputPath, int erosionSize);
+    static void dilate(String &inputPath, String &outputPath, int erosionSize, char verbose);
 
-    static void erode(String &inputPath, String &outputPath, int erosionSize);
+    static void erode(String &inputPath, String &outputPath, int erosionSize, char verbose);
 
-    static void resizeImage(String &inputPath, String &outputPath, int width, int height);
+    static void resizeImage(String &inputPath, String &outputPath, int width, int height, char verbose);
 
-    static void resizeImage(String &inputPath, String &outputPath, double factor);
+    static void resizeImage(String &inputPath, String &outputPath, double factor, char verbose);
 
-    static void lightenDarken(String &inputPath, String &outputPath, double factor);
+    static void lightenDarken(String &inputPath, String &outputPath, double factor, char verbose);
 
-    static void stitch(std::vector<String> *inputPaths, String &outputPath);
+    static void stitch(std::vector<String> *inputPaths, String &outputPath, char verbose);
 
     static void
-    cannyEdgeDetection(String &inputPath, String &outputPath, double threshold1, double threshold2, int apertureSize);
+    cannyEdgeDetection(String &inputPath, String &outputPath, double threshold1, double threshold2, int apertureSize, char verbose);
 
-    static void faceDetection(String &inputPath, String &outputPath, String cascadeModel);
+    static void faceDetection(String &inputPath, String &outputPath, String cascadeModel, char verbose);
 };
 
 #endif
